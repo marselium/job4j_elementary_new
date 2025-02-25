2722a88ea017c4ca91586809fff8aa5f0fff9d1b
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-02-25 16:58:07 +0300

    4.1. Разбор первой программы. [#159033 #354897]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,8 +2,8 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j < 6; j++) {
                 System.out.println(i*j);
             }
         }
