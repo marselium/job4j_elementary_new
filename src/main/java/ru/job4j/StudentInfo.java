46ca3de296db80e2d140d3a021634d5bba3c45bf
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-02-25 21:53:18 +0300

    4.2. Заголовок в файле java [#159036 #354899]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -4,7 +4,7 @@ public class StudentInfo {
     public static void main(String[] args) {
         for (int i = 1; i < 6; i++) {
             for (int j = 1; j < 6; j++) {
-                System.out.println(i*j);
+                System.out.println(i * j);
             }
         }
     }
