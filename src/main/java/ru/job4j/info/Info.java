4c1f25842ee6b2b3d2c18fd2e171dce345e2b501
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-02-25 17:12:37 +0300

    4.2. Заголовок в файле java [#159036 #354899]

---
+++ src/main/java/ru/job4j/info/Info.java
@@ -0,0 +1,12 @@
+package ru.job4j.info;
+
+
+import java.util.Date;
+
+public class Info {
+    public static void main(String[] args) {
+        Date sysdate = new Date();
+        String dateStr = sysdate.toString();
+        System.out.println(dateStr);
+    }
+}
