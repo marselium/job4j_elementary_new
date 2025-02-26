3bc674b1bb145aa4f82c52542d0033b165d896fc
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-02-26 18:28:00 +0300

    4.1. Аргументы [#295521 #356544]

---
+++ src/main/java/ru/job4j/ru/job4j/calculator/ArgumentsMethod.java
@@ -0,0 +1,17 @@
+package ru.job4j.ru.job4j.calculator;
+
+public class ArgumentsMethod {
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void helloAge(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
+    public static void main(String[] args) {
+        String name = "Marsel";
+        ArgumentsMethod.hello(name);
+        int age = 24;
+        ArgumentsMethod.helloAge(name, age);
+    }
+}
