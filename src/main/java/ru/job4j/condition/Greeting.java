b4710290bd59989b75f02dbe3b01482e2eb299f7
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-02-27 21:13:57 +0300

    9.1. String [#237407 #357035]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String java = "I like Java!";
+        String space = " ";
+        String newbie = "But I'm a newbie;";
+        int year = 2025;
+        System.out.println(java + space + newbie + space + year);
+    }
+}
