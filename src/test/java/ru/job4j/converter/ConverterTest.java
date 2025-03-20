d1183093caf3a82170541417212f34d898f7bed3
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-03-20 21:38:23 +0300

    1. Модульные тесты. [#159435 #357582]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert220RubThen2Euro() {
+        float input = 220;
+        float expected = 2;
+        float output = Converter.rubToEur(input);
+        float value = 0.0001f;
+        assertThat(output).isEqualTo(expected,withPrecision(value));
+    }
+
+    @Test
+    void whenConvert200RblThen2Dol() {
+        float input = 200;
+        float exp = 2;
+        float out = Converter.rubToDol(input);
+        assertThat(out).isEqualTo(exp);
+    }
+}
