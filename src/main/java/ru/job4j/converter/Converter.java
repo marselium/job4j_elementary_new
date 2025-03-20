b0fd107cf26ecd5d000aa9532fc30918037bbf55
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-03-20 21:09:07 +0300

    0. Что такое тестирование [#159431 #357256]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,19 +2,27 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubToEur(float val) {
-        float res = val / 110;
-        return res;
+        return val / 110;
     }
 
     public static float rubToDol(float val) {
-        float res = val / 100;
-        return res;
+        return val / 100;
     }
 
     public static void main(String[] args) {
-        float eur = rubToEur(10000);
-        float dol = rubToDol(110000);
+        float eur = rubToEur(11000);
+        float dol = rubToDol(10000);
         System.out.println(eur);
         System.out.println(dol);
+        float inpEur = 220;
+        float expEur = 2;
+        float outEur = rubToEur(inpEur);
+        boolean eurPassed = expEur == outEur;
+        System.out.println("rub to eur converter test : " + eurPassed);
+        float inpDol = 300;
+        float expDol = 3;
+        float outDol = rubToDol(inpDol);
+        boolean dolPassed = outDol == expDol;
+        System.out.println("rub to dollar converter test : " + dolPassed);
     }
 }
