fb373fb3ac0a4d3395c2f1c83e604d9c11029950
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-03-22 15:58:10 +0300

    0. Операции сравнения [#235631 #359419]

---
+++ src/main/java/ru/job4j/condition/Liken.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int fir = 10;
+        int sec = 9;
+
+        boolean rsl = fir > sec;
+        System.out.println(rsl);
+
+        rsl = fir < sec;
+        System.out.println(rsl);
+
+        rsl = fir == sec;
+        System.out.println(rsl);
+    }
+}
