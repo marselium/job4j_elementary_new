135f15527897429a0b52cdb3c68f9085de213f6a
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-03-26 23:11:40 +0300

    6.0.3. Заполнение массива. [#226300 #362473]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,13 @@ public class ArrayDefinition {
         System.out.println("ages length: " + ages.length);
         System.out.println("surnames length: " + surnames.length);
         System.out.println("prices length: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "First";
+        names[1] = "Sec";
+        names[2] = "Third";
+        names[3] = "4rd";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
