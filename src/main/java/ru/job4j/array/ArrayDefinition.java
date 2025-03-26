3c68a2896da9b16e8912115084c1584a3565a72e
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-03-26 22:58:49 +0300

    6.0.2. Размер массива [#333582 #362469]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        System.out.println("ages length: " + ages.length);
+        System.out.println("surnames length: " + surnames.length);
+        System.out.println("prices length: " + prices.length);
     }
 }
