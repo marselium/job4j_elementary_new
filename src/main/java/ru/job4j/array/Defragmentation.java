57025c5ac5e18694240e05e7984b1b8f95c5c021
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-03-31 22:42:09 +0300

    6.8. Метод двух указателей [#225 #364721]

---
+++ src/main/java/ru/job4j/array/Defragmentation.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+public class Defragmentation {
+    public static int[] compress(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == 0) {
+                for (int j =i + 1; j < data.length; j++) {
+                    if (data[j] != 0) {
+                        data[i] = data[j];
+                        data[j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        return data;
+    }
+
+    public static int[] compress2Point(int[] array) {
+        int i = 0;
+        int j = 0;
+        while (j < array.length) {
+            if (array[i] == 0 && array[j] != 0) {
+                array[i] = array[j];
+                array[j] = 0;
+            }
+            if (array[i] != 0) {
+                i++;
+            }
+            j++;
+        }
+        return array;
+    }
+}
---
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class TwoNumberSum {
+    public static int[] getIndexes(int[] array, int target) {
+        int i = 0;
+        int j = 0;
+        int[] arr = new int[2];
+        while (i < array.length) {
+            if ((array[i] + array[j] == target) && (i != j)) {
+                arr[0] = i;
+                arr[1] = j;
+                return arr;
+            }
+            if (j == array.length - 1) {
+                j = i;
+                i++;
+            }
+            j++;
+        }
+        return new int[0];
+    }
+}
