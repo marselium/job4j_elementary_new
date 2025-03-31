98b1ac21b6ec1641b439aeb265126709799bf2b2
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-03-31 21:12:40 +0300

    6.7.4. Выигрышные комбинации в сокобан [#53859 #363972] - оптимизировано

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -33,10 +33,8 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean rsl = false;
         for (int i = 0; i < 5; i++) {
-                if (board[i][i] == 'X') {
-                   if (monoHorizontal(board, i) || monoVertical(board, i)) {
-                       return true;
-                   }
+            if (board[i][i] == 'X' && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                return true;
             }
         }
         return rsl;
