3f75092148d4adcec21687b50260bb8386ccd4d3
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-04-05 21:52:04 +0300

    2.1. Рефакторинг - Площадь треугольника. [#122642 #372438]

--- src/main/java/ru/job4j/condition/Triangle.java
+++ src/main/java/ru/job4j/condition/Triangle.java
@@ -1,7 +1,34 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.*;
+
 public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.first = a;
+        this.second = b;
+        this.third = c;
+    }
+
+    public double semiPerimeter(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
     public static  boolean exist(double ab, double ac, double bc) {
         return ab + ac > bc && ac + bc > ab && ab + bc > ac;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double ab = first.distance(this.second);
+        double bc = second.distance(this.third);
+        double ca = third.distance(this.first);
+        if (exist(ab, bc, ca)) {
+            double p = semiPerimeter(ab, bc, ca);
+            rsl = sqrt(p * (p - ab) * (p - bc) * (p - ca));
+        }
+        return rsl;
     }
 }
--- src/test/java/ru/job4j/condition/TriangleTest.java
+++ src/test/java/ru/job4j/condition/TriangleTest.java
@@ -6,20 +6,23 @@ import static org.assertj.core.api.Assertions.*;
 class TriangleTest {
 
     @Test
-    void when234ThenTrue() {
-        double ab = 2;
-        double bc = 3;
-        double ac = 4;
-        boolean rsl = Triangle.exist(ab, ac, bc);
-        assertThat(rsl).isTrue();
+    void whenA00B40C04henTrue() {
+       Point a = new Point(0, 0);
+       Point b = new Point(4, 0);
+       Point c = new Point(0, 4);
+       Triangle triangle = new Triangle(a, b, c);
+       double rsl = triangle.area();
+       double exp = 8;
+       assertThat(rsl).isEqualTo(exp, withPrecision(0.001));
     }
 
     @Test
-    void when226ThenFalse() {
-        double ab = 2;
-        double bc = 2;
-        double ac = 6;
-        boolean rsl = Triangle.exist(ab, ac, bc);
-        assertThat(rsl).isFalse();
+    void whenTriangleNotExists() {
+         Point a = new Point(2, 4);
+         Point b = new Point(4, 10);
+         Point c = new Point(1, 1);
+         Triangle triangle = new Triangle(a, b, c);
+         double rsl = triangle.area();
+         assertThat(rsl).isEqualTo(-1);
     }
 }
