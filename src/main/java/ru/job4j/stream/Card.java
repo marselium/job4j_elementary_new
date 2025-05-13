de2d59a768ff9e0a0c85c8d3f2d773a486c06a68
Marsel Yumagulov <marseliumagulov@yandex.ru>
2025-05-13 19:47:19 +0300

    9. Генерирование колоды карт. FlatMap [#504873 #396464]

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -11,9 +11,9 @@ public class Card {
 
     @Override
     public String toString() {
-        return "Card{"
-                + "suit=" + suit
-                + ", value=" + value
+        return "Card {"
+                + "suit = " + suit
+                + ", value = " + value
                 + '}';
     }
 
